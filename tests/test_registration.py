d7c3371984d2a5f046f1eca804151972c5ff7642
Dayzer69 <tischin.anton2011@yandex.ru>
2025-09-02 13:26:09 +0300

    7.5 Знакомство с pageactory.Практика работы с page factory

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -6,6 +6,7 @@ from pages.dashboard_page import DashboardPage
 
 @pytest.mark.regression
 @pytest.mark.registration
+@pytest.mark.flaky(reruns=3, reruns_delay=2)
 def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
     registration_page.registration_form.fill('user.name@gmail.com', 'username', 'password')
